8594f4e3cf67e41d8105d557851386659db9e139
mahfuj10 <mahfujglobal@gmail.com>
2022-01-02 15:33:51 +0600

    add firebase file

---
+++ src/components/Firebase/FirebaseConfig/FirebaseConfig.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FirebaseConfig = () => {
+    return (
+        <div>
+            FirebaseConfig
+            FirebaseConfig
+        </div>
+    );
+};
+
+export default FirebaseConfig;
