c13b6dd1e65d6f9fdd73c7b8e4e30c2dded9003e
Iasir-Arafat-riaz <76946854+Iasir-Arafat-riaz@users.noreply.github.com>
2022-01-02 11:52:51 +0600

    navbar added in header branch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
 import './App.css';
 
 function App() {
