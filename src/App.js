1defab503d0460ddd42e78a90370bfb12eb5685a
Iasir-Arafat-riaz <76946854+Iasir-Arafat-riaz@users.noreply.github.com>
2022-01-02 13:11:14 +0600

    home component added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-
 import "./App.css";
 
-import logo from './logo.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
+import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 import Header from "./components/Header/Header";
-
+import Home from "./components/Home/Home/Home";
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Header></Header>
-      <Routes>
-
-      </Routes>
+        <Header></Header>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/Home" element={<Home></Home>}></Route>
+          <Route path="*" element={<NotFound></NotFound>}></Route>
+        </Routes>
       </BrowserRouter>
     </div>
   );
