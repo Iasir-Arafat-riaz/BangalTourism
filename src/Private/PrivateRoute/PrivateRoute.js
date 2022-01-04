7eb108bb9f93785d675d068b4dee00513bfb2faf
Iasir-Arafat-riaz <76946854+Iasir-Arafat-riaz@users.noreply.github.com>
2022-01-05 01:22:17 +0600

    loading component added to private route

--- src/Private/PrivateRoute/PrivateRoute.js
+++ src/Private/PrivateRoute/PrivateRoute.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
+import Loading from '../../components/Shared/Loding/Loading';
 import useAuth from '../../Hooks/useAuth';
 
 const PrivateRoute = ({children}) => {
     const {user}=useAuth()
     const location=useLocation()
+    if(!user.email){
+        return <Loading></Loading>
+    }
     return (
         <div>
             {user.email?children:<Navigate state={{from:location}} to="/login"></Navigate>}
