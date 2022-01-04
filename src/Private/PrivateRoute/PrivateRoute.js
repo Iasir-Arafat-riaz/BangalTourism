86acd253188366d957a3d1d7a3f7c2fb4bd07bf4
Iasir-Arafat-riaz <76946854+Iasir-Arafat-riaz@users.noreply.github.com>
2022-01-05 00:05:24 +0600

    private route redirect issue solved

--- src/Private/PrivateRoute/PrivateRoute.js
+++ src/Private/PrivateRoute/PrivateRoute.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import useAuth from '../../Hooks/useAuth';
 
-const PrivateRoute = ({children,...rest}) => {
+const PrivateRoute = ({children}) => {
     const {user}=useAuth()
     const location=useLocation()
     return (
