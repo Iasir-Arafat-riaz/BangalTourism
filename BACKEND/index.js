1920164534be3350ba423c34c43cea034dce14b6
Iasir-Arafat-riaz <76946854+Iasir-Arafat-riaz@users.noreply.github.com>
2022-01-07 00:59:35 +0600

    final commit

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,188 +1,205 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-const cors = require('cors');
-const stripe = require('stripe')(process.env.STRIPE_SECRET);
-const { MongoClient } = require('mongodb');
-require('dotenv').config();
-const fileUpload = require('express-fileupload');
-const ObjectId = require('mongodb').ObjectId;
+const cors = require("cors");
+const stripe = require("stripe")(
+  "sk_test_51K93ltBcGooWtax9GkAGLr4DEmlZNpm6tUa0SLImClKgGZFpYehHv9XhvOAf5escrbFzko1UJ1bbecG02hdCCZZu00K0yXRB8H"
+);
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const fileUpload = require("express-fileupload");
+const ObjectId = require("mongodb").ObjectId;
 
 // middleare
 app.use(fileUpload());
 app.use(cors());
 app.use(express.json());
 
-
 async function run() {
+  // mongo db uri
 
-    // mongo db uri
+  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.39aol.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-    const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.39aol.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  try {
+    await client.connect();
+    const database = client.db("Tourism");
+    const ourDivions = database.collection("divisions");
+    const ourPlaces = database.collection("places");
+    const usersCollection = database.collection("users");
+    const reviewCollection = database.collection("reviewCollection");
+    const singlePlace = database.collection("singlePlace");
+    const orderCollection = database.collection("orders");
 
+    // get all divisions
+    app.get("/divisions", async (req, res) => {
+      res.send(await ourDivions.find({}).toArray());
+    });
 
-    try {
-        await client.connect();
-        const database = client.db("Tourism");
-        const ourDivions = database.collection('divisions');
-        const ourPlaces = database.collection('places');
-        const usersCollection = database.collection('users');
-        const reviewCollection = database.collection('reviewCollection');
-        const singlePlace = database.collection('singlePlace');
-        const orderCollection = database.collection('orders');
+    // get all places
+    app.get("/places", async (req, res) => {
+      res.send(await ourPlaces.find({}).toArray());
+    });
+    // get all places
+    app.get("/singlePlace", async (req, res) => {
+      res.send(await singlePlace.find({}).toArray());
+    });
 
+    // get user review
+    app.get("/review", async (req, res) => {
+      res.send(await reviewCollection.find({}).toArray());
+    });
+    // get user review
+    app.post("/review", async (req, res) => {
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
+      res.send(result);
+    });
 
-        // get all divisions 
-        app.get('/divisions', async (req, res) => {
-            res.send(await ourDivions.find({}).toArray());
-        })
+    // saver user
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+    // post order
+    app.post("/order", async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
+    });
+    // post place
+    app.post("/place", async (req, res) => {
+      const place = req.body;
+      const result = await ourPlaces.insertOne(place);
+      res.send(result);
+    });
 
-        // get all places 
-        app.get('/places', async (req, res) => {
-            res.send(await ourPlaces.find({}).toArray());
-        });
-        // get all places 
-        app.get('/singlePlace', async (req, res) => {
-            res.send(await singlePlace.find({}).toArray());
-        });
+    // get cart product with email
+    app.get("/order/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await orderCollection.find(query).toArray();
+      res.send(result);
+    });
+    // get cart product with ui
+    app.get("/order/:uid", async (req, res) => {
+      const uid = req.params.email;
+      const query = { uid: uid };
+      const result = await orderCollection.find(query).toArray();
+      res.send(result);
+    });
+    // get cart product with ui
+    app.get("/manageorder", async (req, res) => {
+      res.send(await orderCollection.find({}).toArray());
+    });
 
-        // get user review 
-        app.get('/review', async (req, res) => {
-            res.send(await reviewCollection.find({}).toArray());
-        });
-        // get user review 
-        app.post('/review', async (req, res) => {
-            const review = req.body;
-            const result = await reviewCollection.insertOne(review);
-            res.send(result)
-        });
+    // delete cart api
+    app.delete("/order/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.json(result);
+    });
 
-        // saver user
-        app.post('/users', async (req, res) => {
-            const user = req.body;
-            const result = await usersCollection.insertOne(user);
-            res.send(result);
-        });
-        // post order
-        app.post('/order', async (req, res) => {
-            const order = req.body;
-            const result = await orderCollection.insertOne(order);
-            res.send(result);
-        });
+    // delete tour place
+    app.delete("/tour/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await ourPlaces.deleteOne(query);
+      res.json(result);
+    });
 
-        // get cart product with email
-        app.get('/order/:email', async (req, res) => {
-            const email = req.params.email;
-            const query = { email: email };
-            const result = await orderCollection.find(query).toArray();
-            res.send(result);
-        });
-        // get cart product with ui
-        app.get('/order/:uid', async (req, res) => {
-            const uid = req.params.email;
-            const query = { uid: uid };
-            const result = await orderCollection.find(query).toArray();
-            res.send(result);
-        });
+    // save user google sign
+    app.put("/users", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+    });
 
-        // delete cart api
-        app.delete('/order/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const result = await orderCollection.deleteOne(query);
-            res.json(result);
-        });
+    // single places with _id
+    app.get("/places/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const singleProduct = await ourPlaces.findOne(query);
+      res.send(singleProduct);
+    });
 
-        // save user google sign
-        app.put('/users', async (req, res) => {
-            const user = req.body;
-            const filter = { email: user.email };
-            const options = { upsert: true };
-            const updateDoc = { $set: user };
-            const result = await usersCollection.updateOne(filter, updateDoc, options);
-            res.json(result);
-        });
+    // parent id
+    app.get("/place/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { parentID: id };
+      const place = await ourPlaces.find(query).toArray();
+      res.send(place);
+    });
 
+    // make admin
+    app.put("/users/admin", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const updateDoc = { $set: { role: "admin" } };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.json(result);
+    });
 
-        // single places with _id
-        app.get('/places/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const singleProduct = await ourPlaces.findOne(query);
-            res.send(singleProduct);
-        });
+    // get admin status
+    app.get("/users/:email", async (req, res) => {
+      const user = await usersCollection.findOne({ email: req.params.email });
+      let Admin = false;
+      if (user?.role === "admin") {
+        Admin = true;
+      }
+      res.json({ Admin: Admin });
+    });
 
-        // parent id
-        app.get('/place/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { parentID: id };
-            const place = await ourPlaces.find(query).toArray();
-            res.send(place);
-        });
+    // stripe payment gatway
+    app.post("/payment", async (req, res) => {
+      const paymentInfo = req.body;
+      const amount = paymentInfo.price * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        currency: "usd",
+        amount: amount,
+        payment_method_types: ["card"],
+      });
+      res.json({ clientSecret: paymentIntent.client_secret });
+    });
 
-        // make admin
-        app.put('/users/admin', async (req, res) => {
-            const user = req.body;
-            const filter = { email: user.email };
-            const updateDoc = { $set: { role: "admin" } };
-            const result = await usersCollection.updateOne(filter, updateDoc);
-            res.json(result);
-        });
-
-        // get admin status
-        app.get('/users/:email', async (req, res) => {
-            const user = await usersCollection.findOne({ email: req.params.email });
-            let Admin = false;
-            if (user?.role === 'admin') {
-                Admin = true;
-            };
-            res.json({ Admin: Admin });
-        });
-
-
-        // stripe payment gatway
-        app.post('/createPayment', async (req, res) => {
-            const paymentInfo = req.body;
-            const amount = paymentInfo.price * 100;
-            const paymentIntent = await stripe.paymentIntents.create({
-                currency: 'usd',
-                amount: amount,
-                payment_method_types: ['card']
-            });
-            res.json({ clientSecret: paymentIntent.client_secret })
-        });
-
-        // set payment status
-        app.put('/cartProducts/:email', async (req, res) => {
-            const email = req.params.email;
-            const payment = req.body;
-            const filter = { email: email };
-            const updateDoc = {
-                $set: {
-                    payment: payment
-                }
-            };
-            const result = await orderCollection.updateMany(filter, updateDoc);
-            res.json(result);
-        })
-
-
-    }
-    finally {
-        // await client.close()
-    }
-
+    // set payment status
+    app.put("/cartProducts/:tourId", async (req, res) => {
+      const tourId = req.params.tourId;
+      const payment = req.body;
+      const filter = { tourId: tourId };
+      // const filter = { email: email };
+      const updateDoc = {
+        $set: {
+          isPaid: payment.isPaid,
+        },
+      };
+      const result = await orderCollection.updateOne(filter, updateDoc);
+      res.json(result);
+    });
+  } finally {
+    // await client.close()
+  }
 }
 
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-    res.send('welcome to bangla tourism');
-})
+app.get("/", (req, res) => {
+  res.send("welcome to bangla tourism");
+});
 
 app.listen(port, () => {
-    console.log(`My server is running`);
-})
+  console.log(`My server is running`);
+});
--- src/components/Dashboard/AddPlan/AddPlan.js
+++ src/components/Dashboard/AddPlan/AddPlan.js
@@ -1,37 +1,89 @@
-import axios from 'axios';
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import "./AddPlan.css"
+import axios from "axios";
+import React from "react";
+import { useForm } from "react-hook-form";
+import "./AddPlan.css";
 const AddPlan = () => {
-    const { register, handleSubmit,reset } = useForm();
-    const onSubmit = (data) => {
-      //   const dataobject ={bookingInfo:     }
-        console.log(data)
-        axios.post("",data)
-        .then(respo=>{
-            console.log(respo);
-            if(respo.status==200){
-   reset()
-            }
-        })
-     
-    };
+  const { register, handleSubmit, reset } = useForm();
+  const onSubmit = (data) => {
+    //   const dataobject ={bookingInfo:     }
+    console.log(data);
+            axios.post("https://glacial-shelf-30568.herokuapp.com/place",data)
+            .then(respo=>{
+                console.log(respo);
+                if(respo.status==200){
+       reset()
+                }
+            })
+  };
 
-    return (
-        <div className="add-Packages-Plan">
-        <h2 className="common-header mt-3 text-center">You Can Add New Package Plan</h2>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <input required placeholder="Enter Tour plane ID" type="number" {...register("serviceId")} />
-          <input required placeholder="Enter Tour Plan name " {...register("service")} />
-          <textarea required  placeholder="Enter description about your Tour Plan" {...register("detail")} />
-          
-          <input required placeholder="Enter Approximate cost for your Tour Plan"  type="number" {...register("cost")} />
-          <input required placeholder="You can submit photo, must need valid url  "  {...register("img")} />
-  
-          <input className="bg-danger" type="submit" />
-        </form>
-      </div>
-    );
+  return (
+    <div className="add-Packages-Plan">
+      <h2 className="common-header mt-3 text-center">
+        You Can Add New Package Plan
+      </h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          required
+          placeholder="Enter Tour plane ID"
+          type="number"
+          {...register("placeId")}
+        />
+        <input
+          required
+          placeholder="Enter Tour Plan name "
+          {...register("placeName")}
+        />
+        <textarea
+          required
+          placeholder="Enter description about your Tour Plan"
+          {...register("description")}
+        />
+
+        <input
+          required
+          placeholder="Enter Approximate cost for your Tour Plan"
+          type="number"
+          {...register("price")}
+        />
+        <input
+          required
+          placeholder="enter review counts"
+          type="number"
+          {...register("review")}
+        />
+        <input
+          required
+          placeholder="enter Stay Duration"
+          type="number"
+          {...register("duration")}
+        />
+        <input
+          required
+          placeholder="enter plan rating"
+          type="number"
+          {...register("rating")}
+        />
+        <input
+          required
+          placeholder="enter division Parent id"
+          type="number"
+          {...register("parentID")}
+        />
+        <input
+          required
+          placeholder="You can submit photo, must need valid url  "
+          {...register("image")}
+        />
+        <input
+          required
+          placeholder="Enter Division name"
+          {...register("divisionName")}
+        />
+
+        <input id="addPlanBtn" type="submit" />
+      </form>
+    </div>
+  );
 };
 
 export default AddPlan;
